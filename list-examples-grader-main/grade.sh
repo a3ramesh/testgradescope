008ce983317eaa10f7b9d87354c4e4d8a2856ed6
a3ramesh <130017333+a3ramesh@users.noreply.github.com>
2023-05-24 20:22:05 -0500

    committed

---
+++ list-examples-grader-main/grade.sh
@@ -0,0 +1,28 @@
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
+rm -rf student-submission
+rm -rf grading-area
+
+mkdir grading-area
+
+git clone $1 student-submission
+echo 'Finished cloning'
+
+
+
+# Draw a picture/take notes on the directory structure that's set up after
+# getting to this point
+
+cp -r student-submission/ListExamples.java TestListExamples.java lib grading-area
+
+if[[-f ListExamples.java]]&&[[-f TestListExamples.java]]
+then 
+echo javac ListExamples.java
+
+fi
+# elif
+# then
+# exit
+# fi
+# Then, add here code to compile and run, and do any post-processing of the
+# tests
